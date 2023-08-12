d9ce50145c4dcb63618f245b8f67c04afe945e41
ValentinWach <121336817+ValentinWach@users.noreply.github.com>
2023-08-12 21:08:16 +0200

    Update and rename SendNotification.sol to SendNotification2.sol

--- vwa_firsttests/SendNotification2.sol
+++ vwa_firsttests/SendNotification2.sol
@@ -8,16 +8,20 @@ interface IPUSHCommInterface {
     function sendNotification(address _channel, address _recipient, bytes calldata _identity) external;
 }
 
-contract Owner {
-
+contract SendNotifications2 {
+    uint counter = 0;
     function balanceOf()public view returns(uint){
         return address(this).balance;
     }
 
-    function depositEth() public payable{}
+    function depositEth() payable external {}
+
+    function logging() public payable{
+        counter = counter + 1;
+    }
 
     
-    function sendNotification() payable external {
+    function sendNotification(string memory message) payable external {
          IPUSHCommInterface(0xb3971BCef2D791bc4027BbfedFb47319A4AAaaAa).sendNotification(
             0xFa3D1BD6C0aB6be3A7397F909f645AB0bA0CcCe0, // ???from channel - recommended to set channel via dApp and put it's value -> then once contract is deployed, go back and add the contract address as delegate for your channel
             address(this), // to recipient, put address(this) in case you want Broadcast or Subset. For Targetted put the address to which you want to send
@@ -29,9 +33,9 @@ contract Owner {
                         "+", // segregator
                         "1", // this is payload type: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/payload (1, 3 or 4) = (Broadcast, targetted or subset)
                         "+", // segregator
-                        "Test", // this is notificaiton title
+                        "Balance Update", // this is notificaiton title
                         "+", // segregator
-                        "Hello World" // notification body
+                        message // notification body
                     )
                 )
             )
