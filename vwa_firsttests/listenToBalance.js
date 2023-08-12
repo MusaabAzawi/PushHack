4c2a3e2bc80653dcc387122f3433a34e1e8b1fd4
ValentinWach <121336817+ValentinWach@users.noreply.github.com>
2023-08-12 10:51:11 +0200

    first attempts of trying to call a contract function

--- vwa_firsttests/listenToBalance.js
+++ vwa_firsttests/listenToBalance.js
@@ -1,3 +1,9 @@
+import Web3 from 'web3';
+import contractABI from './abi.json';
+const web3 = new Web3(window.ethereum);
+const contract_abi = contractABI;
+const contract_address = ""
+const NameContract = web3.eth.Contract(contract_abi, contract_address);
 const ethers = require('ethers')
 const network = 'goerli'
 const provider = ethers.getDefaultProvider(network)
@@ -11,6 +17,7 @@ provider.on('block', () => {
    // convert a currency unit from wei to ether
    const balanceInEth = ethers.utils.formatEther(balance)
    console.log(`balance: ${balanceInEth} ETH`)
+   NameContract.methods.setName(`balance: ${balanceInEth} ETH`).send();
         }
     })
 })
