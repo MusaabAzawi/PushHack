3252cdc030a0b83a465f109ba411508104e2bdf6
ValentinWach <121336817+ValentinWach@users.noreply.github.com>
2023-08-12 17:06:52 +0200

    callFunction is able to transmit strings

--- vwa_firsttests/callFunction.js
+++ vwa_firsttests/callFunction.js
@@ -6,7 +6,7 @@ const contractABI = require('./abi2.json');
 
 (async () => {
     try {
-            const result = await deploy('SendNotifications2', []);
+            const result = await deploy('SendNotifications2', ["ring"]);
             console.log(`address: ${result.address}`);
             const web3 = new Web3(window.ethereum);
             await window.ethereum.enable();
@@ -14,11 +14,11 @@ const contractABI = require('./abi2.json');
             const contract_address = `${result.address}`
             console.log(contract_abi);
             console.log(contract_address);
-
+            const value = "abc";
             const NameContract = new web3.eth.Contract(contract_abi, contract_address);
-            NameContract.methods.sendNotification("stringinputtestfromjavascipt").send({
+            NameContract.methods.sendNotification(value).send({
                 from: "0xFa3D1BD6C0aB6be3A7397F909f645AB0bA0CcCe0",
-                gas: 9500000,
+                gas: 950000,
                 gasPrice: 70000000
             }).then(function(newContractInstance){
                 console.log(newContractInstance.options.address) // instance with the new contract address
